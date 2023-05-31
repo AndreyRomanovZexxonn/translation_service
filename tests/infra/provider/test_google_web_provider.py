27b2789a048c780fed2ec321f4bb6ee6bac3f3a9
zexxonn <zexxonn@gmail.com>
2023-05-31 17:16:07 +0400

    WIP remove dependencies

--- tests/infra/provider/test_google_web_provider.py
+++ tests/infra/provider/test_google_web_provider.py
@@ -1,15 +1,9 @@
-import pytest
-
 from src.infra.provider.google.web.constants import Lang
 from src.infra.provider.google.web.models import GoogleTranslatedWord
 from src.infra.provider.google.web.translator import Translator
 
 
-@pytest.mark.asyncio
-async def test_google_web_provider():
+def test_google_web_provider():
     translator = Translator()
-    results = []
     result: GoogleTranslatedWord = await translator.translate("channel", dest=Lang.RU)
-    results.append(result)
-
-    assert results
+    assert result
